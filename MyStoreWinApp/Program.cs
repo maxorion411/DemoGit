bb55e4da426d91449188be506e4c1d780629cf00
maxorion411 <hieupxhe150075@fpt.edu.vn>
2023-10-19 01:30:49 +0700

    ass1 PRN

---
+++ MyStoreWinApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MyStoreWinApp
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+    }
+}
